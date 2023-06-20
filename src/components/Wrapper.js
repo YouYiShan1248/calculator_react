1baf29937b44ec512b87bdc8896b3105cd9eec7d
YouYiShan <1248593898@qq.com>
2023-06-20 10:50:18 +0800

    calculator_react

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -2,47 +2,23 @@ import React, { Component } from 'react'
 import ButtonBox from './ButtonBox'
 import Screen from './Screen'
 import '../style/Wrapper.css'
+import calculate from '../tools/calculate'
 
 export default class Wrapper extends Component {
   state = {
-    result: '',
-    expression: ''
+    total: null,
+    next: null,
+    operation: null
   }
 
   handleClick = (event) => {
-    switch (event) {
-      case 'AC':
-        this.setState({
-          result: '',
-          expression: ''
-        })
-        break
-      case '=':
-        try {
-          const result = eval(this.state.expression).toString()
-          this.setState({
-            result: result,
-            expression: result
-          })
-        } catch (error) {
-          this.setState({
-            result: 'Error',
-            expression: ''
-          })
-        }
-        break
-      default:
-        this.setState((prevState) => ({
-          result: prevState.result + event,
-          expression: prevState.expression + event
-        }))
-    }
+    this.setState(calculate(this.state, event))
   }
 
   render() {
     return (
       <div className="component-app">
-        <Screen value={this.state.result || '0'} />
+        <Screen value={this.state.next || this.state.total || '0'} />
         <ButtonBox clickHandler={this.handleClick} />
       </div>
     )
